bdf8be514b97895e917a75d1edc0dc626a8fb1cd
Anders Olavessen <Anders Olavessen>
2016-03-23 15:44:48 -0500

    tests done

--- src/login-form.js
+++ src/login-form.js
@@ -1,8 +1,36 @@
 
 export default class LoginForm {
-  constructor () {
-    this.login = true;
-    this.password = true;
+  constructor(form, email) {
+    this.form = form;
+
+    this.userDirectory = [
+      { email: `aaron@theironyard.com`, password: `password123` },
+      { email: `admin@google.com`, password: `pandas` },
+      { email, password: `honeycrisp` },
+      // { email: `email@email.com`, password: `pandas`},
+    ];
+  }
 
 
-}
+  validate(email, password) {
+    return this.userDirectory.reduce((prev, current) => {
+      if (current.email === email && current.password === password) {
+        return true;
+      }
+
+      return prev;
+    }, false);
+
+  }
+
+  validateInputs() {
+    const emailInput = this.form.querySelector(`.login-form__email`).value;
+    const passwordInput = this.form.querySelector(`.login-form__password`).value;
+    const valMsg = this.form.querySelector(`.login-form__validation-message`);
+    if (this.validate(emailInput, passwordInput)) {
+      valMsg.innerText = ``;
+    } else {
+      valMsg.innerText = `The credentials are invalid`;
+    }
+  }
+ }
